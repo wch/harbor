6e2232a605c2bf77bcc252c2a5f6f2cb78dac0ba
Winston Chang <winston@stdout.org>
2014-10-21 22:37:08 -0500

    Add docker_pull and docker_run

--- R/docker.R
+++ R/docker.R
@@ -8,3 +8,46 @@
 docker_cmd <- function(host, cmd = NULL, args = NULL, docker_opts = NULL, ...) {
   UseMethod("docker_cmd")
 }
+
+
+#' Pull a docker image onto a host.
+#'
+#' @examples
+#' \dontrun{
+#' docker_pull(localhost(), "debian:testing")
+#' }
+#' @return The \code{host} object.
+#' @export
+docker_pull <- function(host = localhost(), image, ...) {
+  if (is.null(image)) stop("Must specify an image.")
+  docker_cmd(host, "pull", image, ...)
+}
+
+
+#' Run a command in a new container on a host.
+#'
+#' @examples
+#' \dontrun{
+#' docker_run(localhost(), "debian:testing", "echo foo")
+#' #> foo
+#'
+#' # Arguments will be concatenated
+#' docker_run(localhost(), "debian:testing", c("echo foo", "bar"))
+#' #> foo bar
+#'
+#' docker_run(localhost(), "rocker/r-base", c("Rscript", "-e", "1+1"))
+#' #> [1] 2
+#' }
+#' @export
+docker_run <- function(host = localhost(), image = NULL, cmd = NULL,
+                       name = NULL, rm = FALSE,...) {
+
+  if (is.null(image)) stop("Must specify an image.")
+
+  docker_opts <- c(
+    if (rm) "--rm",
+    if (!is.null(name)) sprintf('--name="%s"', name)
+  )
+
+  docker_cmd(host, "run", args = c(image, cmd), docker_opts = docker_opts, ...)
+}
