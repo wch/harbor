53c70bc768fb12c185a68eb17f0543d18f3d489c
Winston Chang <winston@stdout.org>
2014-10-21 22:36:40 -0500

    Don't run examples

--- R/docker.R
+++ R/docker.R
@@ -1,7 +1,9 @@
 #' Run a docker command on a host.
 #'
 #' @examples
+#' \dontrun{
 #' docker_cmd(localhost(), "ps", "-a")
+#' }
 #' @export
 docker_cmd <- function(host, cmd = NULL, args = NULL, docker_opts = NULL, ...) {
   UseMethod("docker_cmd")
